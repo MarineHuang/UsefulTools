363990129603cc702490a96c643602b7cfe44b74
MarineHuang <huang_yx005@163.com>
2020-04-20 17:36:38 +0800

    add utf8 header

--- subtitles/clean_index.py
+++ subtitles/clean_index.py
@@ -1,8 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 import codecs
 import pysrt
 
 
-subs=pysrt.open(u'Alexis Ohanian - How to make a splash in social media.zh-cn.srt')
+subs=pysrt.open(u'不能说的秘密.zh-cn.srt')
 print(len(subs))
 subs.clean_indexes()
-subs.save('Alexis Ohanian - How to make a splash in social media.zh-cn.srt2', encoding='utf-8')
+subs.save(u'不能说的秘密.zh-cn.srt2', encoding='utf-8')
